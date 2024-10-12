41966985aada876d1726f7d899daf8c6e1bdce75
dittygoops <aditya.gup16@gmail.com>
2024-10-12 16:41:08 -0700

    react

---
+++ front-end/firebaseConfig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmieQrqLjTJzAhkLV9VpdLQNuVrUthvks",
+  authDomain: "hackathon-legacyforge.firebaseapp.com",
+  projectId: "hackathon-legacyforge",
+  storageBucket: "hackathon-legacyforge.appspot.com",
+  messagingSenderId: "545824413746",
+  appId: "1:545824413746:web:eb1ef61ff3cc470f7d042b",
+  measurementId: "G-Z4JWFV4NGC"
+};
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
