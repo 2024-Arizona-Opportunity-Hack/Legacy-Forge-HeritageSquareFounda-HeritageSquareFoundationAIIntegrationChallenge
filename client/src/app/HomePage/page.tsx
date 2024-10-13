a2e16abd7c63b905360d3c446702085c168d74c1
ashcodex505 <ashishkurse@gmail.com>
2024-10-12 18:47:49 -0700

    Changes

--- client/src/app/HomePage/page.tsx
+++ client/src/app/HomePage/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import LoginPage from '@/components/LoginPage';
 import React from 'react';
+import "../../App.css"
 const HomePage = ()=> {
     return (
     <>
-    <LoginPage/>
+        <LoginPage/>
     
 
     </>
--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,8 +1,40 @@
+"use client"
+import { auth, googleProvider } from '@/firebaseConfig';
+import { signInWithPopup } from 'firebase/auth';
+import React, { useState } from 'react';
+
+import '../../src/App.css'
+
 
 const LoginPage = () => {
+    const [loading, setLoading] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const authWithGoogle = async () => {
+        setLoading(true);
+        try {
+            await signInWithPopup(auth, googleProvider);
+            setIsAuthenticated(true);
+        } catch {
+            console.log("Invalid");
+        } finally {
+          setLoading(false);
+        }
+      }
     return (
         <>
-        f
+         <div>
+        <div className={`app-container ${isAuthenticated ? '' : 'blurred'}`}>
+            <div className='chat-interface'>
+            <h1>Logged in</h1>
+            </div>
+        </div>
+
+        <div className='login-overlay'>
+            {isAuthenticated ? ( <></> ) : (
+            <button onClick={ () => authWithGoogle() } disabled={ loading }>{loading ? "Logging in..." : "Log in with Google"}</button>
+            )}
+        </div>
+    </div>
         </>
     ) 
 }
