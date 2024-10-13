d7ff6afea5b02a9c4042d56cada1e4ec8083cbf6
ashcodex505 <ashishkurse@gmail.com>
2024-10-12 17:29:00 -0700

    Initial commit

---
+++ client/src/app/HomePage/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import LoginPage from '@/components/LoginPage';
+import React from 'react';
+const HomePage = ()=> {
+    return (
+    <>
+    <LoginPage/>
+    
+
+    </>
+    )
+
+}
+export default HomePage;
---
+++ client/src/components/LoginPage.tsx
@@ -0,0 +1,9 @@
+
+const LoginPage = () => {
+    return (
+        <>
+        f
+        </>
+    ) 
+}
+export default LoginPage;
